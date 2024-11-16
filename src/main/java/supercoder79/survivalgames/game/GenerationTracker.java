efe0d911e3e6852a83527c1a97d661341370af6d
Patbox <39821509+Patbox@users.noreply.github.com>
2024-11-16 11:01:11 +0100

    Fix crash hopefully

--- src/main/java/supercoder79/survivalgames/game/GenerationTracker.java
+++ src/main/java/supercoder79/survivalgames/game/GenerationTracker.java
@@ -4,27 +4,22 @@ import net.minecraft.util.math.BlockPos;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.concurrent.CopyOnWriteArraySet;
 import java.util.function.Consumer;
 import java.util.function.Predicate;
 
 public final class GenerationTracker {
-    private final Set<BlockPos> redstoneTracked = new HashSet<>();
+    private final CopyOnWriteArraySet<BlockPos> redstoneTracked = new CopyOnWriteArraySet<>();
 
     public void addRedstoneTracked(BlockPos pos) {
-        synchronized (this.redstoneTracked) {
-            this.redstoneTracked.add(pos);
-        }
+        this.redstoneTracked.add(pos);
     }
 
     public void iterateRedstoneTracked(Predicate<BlockPos> consumer) {
-        synchronized (this.redstoneTracked) {
-            this.redstoneTracked.removeIf(consumer);
-        }
+        this.redstoneTracked.removeIf(consumer);
     }
 
     public synchronized void removeRedstoneTracked(BlockPos pos) {
-        synchronized (this.redstoneTracked) {
-            this.redstoneTracked.remove(pos);
-        }
+        this.redstoneTracked.remove(pos);
     }
 }
